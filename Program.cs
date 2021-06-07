56d6b3b5e0628b0fe880da517cf0068c52024605
dallums <derek.allums@gmail.com>
2021-06-07 07:19:10 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logistic_Regression_From_Scratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
