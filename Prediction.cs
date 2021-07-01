9fa8bee313c8fc71c0c32a8f2e36c857619028e2
dallums <derek.allums@gmail.com>
2021-07-01 07:45:48 +0200

    Adding prediction class

---
+++ Prediction.cs
@@ -0,0 +1,13 @@
+namespace Logistic_Regression_From_Scratch
+{
+    public class Prediction
+    {
+        // TODO: build out. Idea is make single prediction from row and model (another row)
+        public Prediction() {}
+
+        public static  decimal makePrediction()
+        {
+            return 0m;
+        }
+    }
+}
