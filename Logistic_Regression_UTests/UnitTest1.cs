657595ba081756634b7a8e9a016dd5183c775eb4
dallums <derek.allums@gmail.com>
2021-07-01 07:12:22 +0200

    Adding UTest project

---
+++ Logistic_Regression_UTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Logistic_Regression_UTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
